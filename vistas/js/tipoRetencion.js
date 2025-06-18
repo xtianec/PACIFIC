76b0a7dfa8f1bc43a303a8bcbc71c325772e3953
xtianec <espino.cristhian@gmail.com>
2025-06-18 13:03:38 -0500

    Apply categoriaCliente style to all scripts

--- vistas/js/tipoRetencion.js
+++ vistas/js/tipoRetencion.js
@@ -5,14 +5,7 @@ $(function () {
     ajax: {
       url: base + 'controlador/' + ctrl + '?op=listar',
       type: 'GET',
-      dataSrc: function (json) {
-        const data = json.data || json.aaData || [];
-        if (data.length && $('#tblHead').children().length === 0) {
-          const headers = Object.keys(data[0]).map(k => `<th>${k}</th>`).join('');
-          $('#tblHead').html('<tr>' + headers + '</tr>');
-        }
-        return data;
-      }
+      dataSrc: 'data'
     }
   });
 
